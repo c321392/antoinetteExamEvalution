6bd9935e2bdeb82abc49c71588c57e213150e44f
c321392 <aventer@inspiredtesting.com>
2022-05-15 17:56:02 +0200

    THird COmmit

--- antoinetteExamEvalution/src/test/java/testsDemoWebShop/TestsDemoWebShop.java
+++ antoinetteExamEvalution/src/test/java/testsDemoWebShop/TestsDemoWebShop.java
@@ -2,4 +2,74 @@ package testsDemoWebShop;
 
 public class TestsDemoWebShop {
 
+	//User Story 1: search using the top menu bar
+		//  GIVEN
+		//		Given the shopper is on the Landing page
+		//	WHEN
+		//		Using the Top Menu Bar
+		//	THEN
+		//		The selected category page will be displayed
+		//
+	public void GIVEN_ShopperOnLandingPage_WHEN_TopMenuBar_THEN_SelectedCategoryPageDisplayed() {
+		
+	}
+	
+	//User Story 2: browse using the categories list
+	//  GIVEN
+	//		Given the shopper is on the Landing page
+	//	WHEN
+	//		Using the Categories List
+	//	THEN
+	//		The selected category page will be displayed
+	//
+	public void GIVEN_ShopperOnLandingPage_WHEN_CategoriesList_THEN_SelectedCategoryPageDisplayed() {
+	
+}
+	
+	//User Story 3: go to cart and update quantity
+	//  GIVEN
+	//		Given the cart is not empty
+	//	WHEN
+	//		Go to cart
+	//	THEN
+	//		Then update the quantity
+	//
+	public void GIVEN_CartnotEmpty_WHEN_GoToCart_THEN_UpdateQty() {
+
+}
+	
+	//User Story 4: remove item from cart
+	//  GIVEN
+	//		Given the cart is not empty
+	//	WHEN
+	//		Go to cart
+	//	THEN
+	//		Then remove the item
+	//
+	public void GIVEN_CartnotEmpty_WHEN_GoToCart_THEN_Remove() {
+
+}	
+	//User Story 5: estimate shipping
+	//  GIVEN
+	//		Given the cart is not empty
+	//	WHEN
+	//		Go to cart
+	//	THEN
+	//		Then perform Estimate Shipping
+	//
+	public void GIVEN_CartnotEmpty_WHEN_GoToCart_THEN_EstimateSHipping() {
+
+}	
+	//User Story 6: add to your compare list
+	//  GIVEN
+	//		Given the shopper is on the Landing page
+	//	WHEN
+	//		Selecting two items out of same categorie
+	//	THEN
+	//		Then you compare items
+	//
+	public void GIVEN_ShopperOnLandingPage_WHEN_SelectTwoItems_THEN_CompareItems() {
+
+}
+	
 }
